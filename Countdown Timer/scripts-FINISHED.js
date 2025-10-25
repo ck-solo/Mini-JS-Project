58c26f636cccd39cd0faa0ffd400b3eea8233e77
Lucy <solouser019@gmail.com>
2025-10-25 13:34:14 +0000

    rewise and rewrite the code

--- Countdown Timer/scripts-FINISHED.js
+++ Countdown Timer/scripts-FINISHED.js
@@ -3,6 +3,7 @@ const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
 
+
 function timer(seconds) {
   // clear any existing timers
   clearInterval(countdown);
